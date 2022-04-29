366cebbb2dc84cbc18d68e9ed7821b70a0c39a4b
Alex Neep <alexander@thesolesupplier.co.uk>
2022-04-29 16:54:00 +0100

    Add a random joke to joke index route

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,3 +1,30 @@
-export default function jokesIndexRoute() {
-  return <div>Here's a random joke</div>;
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+
+import { db } from "~/utils/db.server";
+import type { Joke } from "@prisma/client";
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  const data: LoaderData = { joke: randomJoke };
+  return json(data);
+};
+
+export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>();
+  return (
+    <div>
+      <h1>Here's a random joke</h1>
+      <h2>{data.joke.name}</h2>
+      <p>{data.joke.content}</p>
+    </div>
+  );
 }
