2f0f8a9997f2a8d9aa0204934b03c4f93b6ea35a
Alex Neep <alexander@thesolesupplier.co.uk>
2022-04-29 16:54:11 +0100

    Show chosen joke to user

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,28 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+
+import { db } from "~/utils/db.server";
+import type { Joke } from "@prisma/client";
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) throw new Error("No joke found");
+  const data: LoaderData = { joke };
+  return json(data);
+};
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div>
-      <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>Here's your hilarious joke: {data.joke.name}</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
     </div>
   );
 }
