f42326ff98e14c156bf9352f68de9797eb641cfc
Alex Neep <alexander@thesolesupplier.co.uk>
2022-05-06 17:20:05 +0100

    Add error handling to adding a new joke

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,5 +1,38 @@
-import { ActionFunction, redirect } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import type { ActionFunction } from "@remix-run/node";
 import { db } from "~/utils/db.server";
+import { useActionData } from "@remix-run/react";
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    name?: string | undefined;
+    content?: string | undefined;
+  };
+  fields?: {
+    name: string;
+    content: string;
+  };
+};
+
+function validateJokeName(name: string) {
+  if (name.length < 3) {
+    return "That name is just too damn short";
+  }
+  if (name.length > 10) {
+    return "No good joke has such a long name. 10 characters or less please";
+  }
+}
+
+function validateJokeContent(content: string) {
+  if (content.length === 0) {
+    return "That joke is just too short";
+  }
+  if (content.length > 240) {
+    return "Wow that was a long (boring?) joke. Please get to the chase a bit quicker. 240 characters or less please";
+  }
+}
+const badRequest = (data: ActionData) => json(data, { status: 400 });
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
@@ -7,15 +40,26 @@ export const action: ActionFunction = async ({ request }) => {
   const content = form.get("content");
 
   if (typeof name !== "string" || typeof content !== "string") {
-    throw new Error(`Form not submitted correctly.`);
+    return badRequest({ formError: "Form not submitted properly" });
   }
 
+  const fieldErrors = {
+    name: validateJokeName(name),
+    content: validateJokeContent(content),
+  };
+
   const fields = { name, content };
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields });
+  }
+
   const joke = await db.joke.create({ data: fields });
   return redirect(`/jokes/${joke.id}`);
 };
 
-export default function newJokesRoute() {
+export default function NewJokesRoute() {
+  const actionData = useActionData<ActionData>();
+
   return (
     <div>
       <h2>Add your joke</h2>
@@ -23,13 +67,23 @@ export default function newJokesRoute() {
         <div>
           <label>
             Name:
-            <input type="text" name="name" />
+            <input
+              type="text"
+              defaultValue={actionData?.fields?.name}
+              name="name"
+            />
           </label>
+          {actionData?.fieldErrors?.name ? (
+            <p>{actionData.fieldErrors.name}</p>
+          ) : null}
         </div>
         <label>
           Joke:
-          <textarea name="content" />
+          <textarea defaultValue={actionData?.fields?.content} name="content" />
         </label>
+        {actionData?.fieldErrors?.content ? (
+          <p>{actionData.fieldErrors.content}</p>
+        ) : null}
         <button type="submit">Add</button>
       </form>
     </div>
